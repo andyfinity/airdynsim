943f5d93ffb41137bc5c2deb963325cb1be538c5
wildman1324 <s.brian.clark@gmail.com>
2013-12-08 15:33:12 -0800

    FInished adding p.934 equations

--- equations.py
+++ equations.py
@@ -36,10 +36,38 @@ B = {}
 
 
 
+A['x,alpha'] = (rho * S_w * l_ref)/(2*W/g)*C['X,aplha']
+A['z,alpha'] = (rho * S_w * l_ref)/(2*W/g)*C['Z,aplha']
+A['m,alpha'] = (rho * S_w * l_ref)/(2*W/g)*C['m,aplha']
+
+A['y,beta'] = (rho * S_w * l_ref)/(2*W/g)*C['Y,beta']
+A['l,beta'] = (rho * S_w * b_w * l_ref^2)/(2*I['xx_b'])*C['l,beta']
+A['n,beta'] = (rho * S_w * b_w * l_ref^2)/(2*I['zz_b'])*C['n,beta']
+
+A['l,alpha'] = (rho * S_w * b_w * l_ref^2)/(2*I['xx_b'])*C['l,aplha']
+A['m,beta'] = (rho * S_w * cbar_w * l_ref^2)/(2*I['yy_b'])*C['m,beta']
+A['n,alpha'] = (rho * S_w * b_w * l_ref^2)/(2*I['zz_b'])*C['n,aplha']
+
+A['y,pswoosh'] = (rho * S_w * b_w)/(4*W/g)*C['Y,pbar']
+A['l,pswoosh'] = (rho * S_w * b_w^2 * l_ref)/(4*I['xx_b'])*C['l,pbar']
+A['n,pswoosh'] = (rho * S_w * B_w^2 * l_ref)/(4*I['zz_b'])*C['n,pbar']
+
+A['x,qswoosh'] = (rho * S_w * cbar_w)/(4*W/g)*C['X,qbar']
+A['z,qswoosh'] = (rho * S_w * cbar_w)/(4*W/g)*C['Z,qbar']
+A['m,qswoosh'] = (rho * S_w * cbar_w^2 * l_ref)/(4*I['yy_b'])*C['m,qbar']
+
+A['y,rswoosh'] = (rho * S_w * b_w)/(4*W/g)*C['Y,rbar']
+A['l,rswoosh'] = (rho * S_w * b_w^2 * l_ref)/(4*I['xx_b'])*C['l,rbar']
+A['n,rswoosh'] = (rho * S_w * b_w^2 * l_ref)/(4*I['zz_b'])*C['n,rbar']
+
+D['y,delta_a'] = (rho * S_w * l_ref)/(2*W/g)*C['Y,delta_a']
+D['l,delta_a'] = (rho * S_w * l_ref^2)/(2*I['xx_b'])*C['l,delta_a']
+D['n,delta_a'] = (rho * S_w * l_ref^2)/(2*I['zz_b'])*C['n,delta_a']
 
 D['x,delta_e'] = (rho * S_w * l_ref)/(2*W/g)*C['X,delta_e']
 D['z,delta_e'] = (rho * S_w * l_ref)/(2*W/g)*C['Z,delta_e']
 D['m,delta_e'] = (rho * S_w * cbar_w * l_ref**2)/(2*I['yy_b'])*C['m,delta_e']
+
 D['y,delta_r'] = (rho * S_w * l_ref)/(2*W/g)*C['Y,delta_r']
 D['l,delta_r'] = (rho * S_w * b_w * l_ref**2)/(2*I['xx_b'])*C['l,delta_r']
 D['n,delta_r'] = (rho * S_w * b_w * l_ref**2)/(2*I['zz_b'])*C['n,delta_r']
